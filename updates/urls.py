b78830705a4b3015d94335cf2a59c6a0d275ebff
Jang-Jaewon <jewon119@gmail.com>
2022-10-24 09:46:21 +0900

    Fix : cbv로 api 변환 및 JsonResponseMixin 추가

--- updates/urls.py
+++ updates/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import json_example_view
+from .views import json_example_view, JsonCBV1, JsonCBV2
 
 
 urlpatterns = [
     path('json/example', json_example_view),
+    path('json/cbv1', JsonCBV1.as_view()),
+    path('json/cbv2', JsonCBV2.as_view()),
 ]
--- updates/views.py
+++ updates/views.py
@@ -1,7 +1,9 @@
 import json
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
 
+from mixins import JsonResponseMixin
 from .models import Update
 
 
@@ -15,3 +17,20 @@ def json_example_view(request):
     json_data = json.dumps(data)
     return HttpResponse(json_data, content_type="application/json")
 
+
+class JsonCBV1(View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+        return JsonResponse(data)
+
+
+class JsonCBV2(JsonResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+        return self.render_to_json_response(data)
