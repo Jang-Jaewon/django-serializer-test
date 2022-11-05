88022c0db55829f58302eb09310424ac053072bf
Jang-Jaewon <jewon119@gmail.com>
2022-11-05 16:16:49 +0900

    Fix : SerializedDetailView, SerializedListView 테스트

--- updates/urls.py
+++ updates/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from .views import json_example_view, JsonCBV1, JsonCBV2
+from .views import (
+    json_example_view,
+    JsonCBV1,
+    JsonCBV2,
+    SerializedListView,
+    SerializedDetailView
+)
 
 
 urlpatterns = [
     path('json/example', json_example_view),
     path('json/cbv1', JsonCBV1.as_view()),
     path('json/cbv2', JsonCBV2.as_view()),
+    path('json/serialized/list', SerializedListView.as_view()),
+    path('json/serialized/detail', SerializedDetailView.as_view()),
 ]
--- updates/views.py
+++ updates/views.py
@@ -1,4 +1,6 @@
 import json
+
+from django.core.serializers import serialize
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
@@ -34,3 +36,29 @@ class JsonCBV2(JsonResponseMixin, View):
             "content": "Some new content"
         }
         return self.render_to_json_response(data)
+
+
+class SerializedDetailView(View):
+    def get(self, request, *args, **kwargs):
+        obj = Update.objects.get(id=1)
+        json_data = serialize("json", [obj,], fields=("user", "content"))
+        print(json_data)
+
+        return HttpResponse(json_data, content_type="application/json")
+
+
+class SerializedListView(View):
+    def get(self, request, *args, **kwargs):
+        qs = Update.objects.all()
+        # data = serialize("json", qs, fields=("user", "content"))
+        json_data = serialize("json", qs)
+        print(json_data)
+        # data = {
+        #     "user": obj.user.username,
+        #     "content": obj.content
+        # }
+        # json_data = json.dumps(data)
+
+        # console => python manage.py dumpdata updates.Update --format json --indent 4
+
+        return HttpResponse(json_data, content_type="application/json")
